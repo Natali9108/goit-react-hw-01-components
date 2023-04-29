612c51147d3ebe0e85baf5bfb795054eb16459b7
Natali <114168847+Natali9108@users.noreply.github.com>
2023-04-29 23:47:53 +0300

    fix

--- src/components/FriendList/FrendItem.styled.jsx
+++ src/components/FriendList/FrendItem.styled.jsx
@@ -5,8 +5,9 @@ export const Item = styled.li`
   align-items: center;
   gap: 8px;
   padding: 20px;
-  border-radius: 3px;
+  border-radius: 5px;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2), 0 4px 6px rgba(0, 0, 0, 0.2);
+  background-color: ${props => props.theme.colors.primaryWhite};
 `;
 
 export const Status = styled.span`
